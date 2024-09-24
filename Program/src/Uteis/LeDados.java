21979755eeafc07d34dbaeedda8f301e1723b9dc
EMANUELLA <emanuella.portugal@centi.com.br>
2024-09-24 11:10:50 -0300

    .

---
+++ Program/src/Uteis/LeDados.java
@@ -0,0 +1,28 @@
+package Uteis;
+
+import java.util.Scanner;
+
+public class LeDados {
+
+	public static int LeNumeroint(String msg) {
+
+		Scanner sc = new Scanner(System.in);
+		int retorno;
+
+		System.out.println(msg + ".:");
+		retorno = sc.nextInt();
+		return retorno;
+
+	}
+
+	public static String LeString(String msg) {
+		Scanner sc = new Scanner(System.in);
+
+		String retorno;
+		System.out.println(msg);
+
+		retorno = sc.next();
+		return retorno;
+	}
+
+}
